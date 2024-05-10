81f58908db43e7f39e9866242c6ee0fbc8a5e050
Dina <dcojocaru3@gmail.com>
2024-05-10 10:13:22 -0700

    US005 is not done yet!

---
+++ src/test/java/com/fleet/pages/VehicleModelPage.java
@@ -0,0 +1,24 @@
+package com.fleet.pages;
+
+import com.fleet.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class VehicleModelPage extends BasePage {
+
+    @FindBy(xpath = "(//div[@id='main-menu']/ul/li[2]//div/ul/li//span)[9]")
+    public WebElement vehicleModelBtnManager;
+
+    @FindBy(xpath = "//div[@id=main-menu]/ul/li[1]/div/div/ul/li[9]/a/span")
+    public WebElement vehicleModelBtnDriver;
+
+    @FindBy(xpath = "//div[@class='grid-container']//table/thead")
+    public List<WebElement> vehicleModelColumn;
+    @FindBy (xpath = "//div[.='You do not have permission to perform this action.']")
+    public WebElement driverErrorMessage;
+
+
+}
