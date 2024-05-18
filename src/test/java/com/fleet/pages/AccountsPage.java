ca934064dc8db271f4055267d8409e5677bf6166
marcelavazquez <vazquezmarcela13@gmail.com>
2024-05-18 11:28:41 -0600

    US012

--- src/test/java/com/fleet/pages/AccountsPage.java
+++ src/test/java/com/fleet/pages/AccountsPage.java
@@ -1,11 +1,24 @@
 package com.fleet.pages;
 
+import com.fleet.utilities.Driver;
+import io.cucumber.java.bs.A;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v121.fedcm.model.Account;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class AccountsPage {
+import java.util.List;
 
-    @FindBy (xpath ="(//a[@href='/account'])[3]/span")
-    public WebElement accountsBttn;
+public class AccountsPage extends BasePage {
 
+    public AccountsPage (){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy (xpath = "//span[@class='filter-items']")
+    public List<WebElement> filterRow;
+
+    @FindBy (xpath = "//i[@class='fa-filter hide-text']")
+    public WebElement filterBtn;
 }
